711a7a6259d1b013785e359469259c00be93c711
julio <32973371+juliogdomingues@users.noreply.github.com>
2023-08-29 20:39:34 +0000

    corrections in Makefile e fibonacci logic

--- lab02/src/fibonacci.cpp
+++ lab02/src/fibonacci.cpp
@@ -3,13 +3,14 @@
 #include "../include/fibonacci.hpp"
 
 int fibonacci_iterativo(int n) {
+    if (n <= 0) {
+        return 0;  // Tratar
+    } else if (n == 1 || n == 2) {
+        return 1;  // Tratar?
+    }
     int x1 = 1, x2 = 1;
     int x;
-    for (int i = 0; i < n; i++) {
-        if (i < 3) {
-            x = 1;
-            continue;
-        }
+    for (int i = 3; i <= n; i++) {
         x = x1 + x2;
         x2 = x1;
         x1 = x;
@@ -20,6 +21,6 @@ int fibonacci_iterativo(int n) {
 int fibonacci_recursivo (int n) {
     if (n == 0 || n == 1) return n;
     else {
-        return fibonacci_recursivo(n) + fibonacci_recursivo(n-1);
+        return fibonacci_recursivo(n-1) + fibonacci_recursivo(n-2);
     }
 }
