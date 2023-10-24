56884e791d3c314ccadb192d88a4389c520a8946
Julio <jgdjulio@gmail.com>
2023-10-24 19:19:02 -0300

    lab08 main

---
+++ lab08/src/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "../include/heap.hpp"
+#include "../include/unionFind.hpp"
+
+int main() {
+    int n;
+    std::cin >> n;
+
+    Heap minHeap(n);
+
+    for (int i = 0; i < n; ++i) {
+        int num;
+        std::cin >> num;
+        minHeap.Inserir(num);
+    }
+
+    while (!minHeap.Vazio()) {
+        std::cout << minHeap.Remover() << " ";
+    }
+    
+    std::cout << std::endl;
+
+    return 0;
+}
