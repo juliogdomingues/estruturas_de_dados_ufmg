a866d549bab6f02de183b20714a582fa98072fad
julio <32973371+juliogdomingues@users.noreply.github.com>
2023-11-19 23:43:26 -0300

    Add files via upload

---
+++ lab10/src/set.cpp
@@ -0,0 +1,49 @@
+#include "../include/Set.hpp"
+
+StringSet::StringSet(int tamanho) {
+    
+}
+
+StringSet::~StringSet() {
+    delete [] tabela;
+}
+
+void StringSet::Inserir(string s) {
+    
+}
+
+void StringSet::Remover(string s) {
+    
+}
+
+bool StringSet::Pertence(string s) {
+    
+}
+
+StringSet* StringSet::ntersecao(StringSet* S) {
+    
+}
+
+StringSet* StringSet::Uniao(StringSet* S) {
+    
+}
+
+StringSet* StringSet::DiferencaSimetrica(StringSet* S) {
+    
+}
+
+void StringSet::Imprimir() {
+    
+}
+
+int StringSet::Hash(string s) {
+    
+}
+
+void StringSet::Rehash(int pos) {
+    
+}
+
+void StringSet::Resize(size_t tamanho) {
+    
+} 
